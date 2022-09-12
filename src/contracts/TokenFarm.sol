f2a21204d6b276e66ca7f1782d0aa0175fd1a74a
ryojiroakiyama <ryojiro25@gmail.com>
2022-09-12 19:50:28 +0900

    stake

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -9,8 +9,39 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    // 7. これまでにステーキングを行ったすべてのアドレスを追跡する配列を作成
+    address[] public stakers;
+
+    // 4.投資家のアドレスと彼らのステーキングしたトークンの量を紐づける mapping を作成
+    mapping(address => uint256) public stakingBalance;
+
+    // 6. 投資家のアドレスをもとに彼らがステーキングを行ったか否かを紐づける mapping を作成
+    mapping(address => bool) public hasStaked;
+
+    // 10. 投資家の最新のステイタスを記録するマッピングを作成
+    mapping(address => bool) public isStaking;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+    }
+
+    // 1.ステーキング機能を作成する
+    function stakeTokens(uint256 _amount) public {
+        // 2. ステーキングされるトークンが0以上あることを確認
+        require(_amount > 0, "amount can't be 0");
+        // 3. 投資家のトークンを TokenFarm.sol に移動させる
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // 5. ステーキングされたトークンの残高を更新する
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // 8. 投資家がまだステークしていない場合のみ、彼らをstakers配列に追加する
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+        // 9. ステーキングステータスの更新
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 }
--- test/TokenFarm_test.js
+++ test/TokenFarm_test.js
@@ -1,17 +1,12 @@
 // TokenFarm_test.js
-
-// 使用するスマートコントラクトをインポートする
 const DappToken = artifacts.require(`DappToken`);
 const DaiToken = artifacts.require(`DaiToken`);
 const TokenFarm = artifacts.require(`TokenFarm`);
 
-// chai のテストライブラリ・フレームワークを読み込む
-const { assert } = require("chai");
 require(`chai`)
   .use(require("chai-as-promised"))
   .should();
 
-// 任意のETHの値をWeiに変換する関数
 function tokens(n) {
   return web3.utils.toWei(n, "ether");
 }
@@ -20,18 +15,17 @@ contract("TokenFarm", ([owner, investor]) => {
   let daiToken, dappToken, tokenFarm;
 
   before(async () => {
-    //コントラクトの読み込み
+    // コントラクトを読み込む
     daiToken = await DaiToken.new();
     dappToken = await DappToken.new();
     tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address);
 
-    //全てのDappトークンをファームに移動する(1 million)
+    // 全てのDappトークンをファームに移動する(1 million)
     await dappToken.transfer(tokenFarm.address, tokens("1000000"));
 
     await daiToken.transfer(investor, tokens("100"), { from: owner });
   });
 
-  // DaiToken
   describe("Mock DAI deployment", async () => {
     // テスト1
     it("has a name", async () => {
@@ -40,7 +34,6 @@ contract("TokenFarm", ([owner, investor]) => {
     });
   });
 
-  // DappToken
   describe("Dapp Token deployment", async () => {
     // テスト2
     it("has a name", async () => {
@@ -49,18 +42,67 @@ contract("TokenFarm", ([owner, investor]) => {
     });
   });
 
-  // TokenFarm
   describe("Token Farm deployment", async () => {
     // テスト3
     it("has a name", async () => {
       const name = await tokenFarm.name();
       assert.equal(name, "Dapp Token Farm");
     });
-
     // テスト4
     it("contract has tokens", async () => {
       let balance = await dappToken.balanceOf(tokenFarm.address);
       assert.equal(balance.toString(), tokens("1000000"));
+    });
+  });
+  // ----- 追加するテストコード ------ //
+  describe("Farming tokens", async () => {
+    it("rewards investors for staking mDai tokens", async () => {
+      let result;
+      // テスト5. ステーキングの前に投資家の残高を確認する
+      result = await daiToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor Mock DAI wallet balance correct before staking"
+      );
+
+      // テスト6. 偽のDAIトークンを確認する
+      await daiToken.approve(tokenFarm.address, tokens("100"), {
+        from: investor,
+      });
+      await tokenFarm.stakeTokens(tokens("100"), { from: investor });
+
+      // テスト7. ステーキング後の投資家の残高を確認する
+      result = await daiToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("0"),
+        "investor Mock DAI wallet balance correct after staking"
+      );
+
+      // テスト8. ステーキング後のTokenFarmの残高を確認する
+      result = await daiToken.balanceOf(tokenFarm.address);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "Token Farm Mock DAI balance correct after staking"
+      );
+
+      // テスト9. 投資家がTokenFarmにステーキングした残高を確認する
+      result = await tokenFarm.stakingBalance(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor staking balance correct after staking"
+      );
+
+      // テスト10. ステーキングを行った投資家の状態を確認する
+      result = await tokenFarm.isStaking(investor);
+      assert.equal(
+        result.toString(),
+        "true",
+        "investor staking status correct after staking"
+      );
     });
   });
 });
